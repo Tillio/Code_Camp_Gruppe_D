85e1be75ddca671bcb932f3b32418896cfb6c875
Till Pudack <Till.Pudack@gmail.com>
2022-01-14 15:17:27 +0100

    implement firebase

--- app/src/main/java/com/example/group_d/ui/login/LoggedInUserView.kt
+++ app/src/main/java/com/example/group_d/ui/login/LoggedInUserView.kt
@@ -1,9 +1,12 @@
 package com.example.group_d.ui.login
 
+import com.google.firebase.auth.FirebaseUser
+
 /**
  * User details post authentication that is exposed to the UI
  */
 data class LoggedInUserView(
-    val displayName: String
+    val displayName: String,
+    val firebaseUser: FirebaseUser
     //... other data fields that may be accessible to the UI
 )
