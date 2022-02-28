79f6ce59e4d63e2d06eb05cb41ecddeb9bb5b7eb
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-02-28 19:14:39 +0100

    implemented simple rotation of compass needle

--- app/src/main/java/com/example/group_d/ui/main/ingame/CompassFragment.kt
+++ app/src/main/java/com/example/group_d/ui/main/ingame/CompassFragment.kt
@@ -1,5 +1,10 @@
 package com.example.group_d.ui.main.ingame
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +14,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.group_d.databinding.CompassFragmentBinding
 
-class CompassFragment : Fragment() {
+class CompassFragment : Fragment(), SensorEventListener {
     private lateinit var compassViewModel: CompassViewModel
     private var _binding: CompassFragmentBinding? = null
 
     private lateinit var compassNeedle: ImageView
+
+    private var sensorManager: SensorManager? = null
+    private var rotVecSensor: Sensor? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,13 +36,38 @@ class CompassFragment : Fragment() {
         _binding = CompassFragmentBinding.inflate(inflater, container, false)
         val root = binding.root
         compassNeedle = binding.compassNeedle
-        compassNeedle.rotation = 90F
+
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+        rotVecSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
 
         return root
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(this, rotVecSensor, SensorManager.SENSOR_DELAY_UI)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val rotMat = FloatArray(9)
+        SensorManager.getRotationMatrixFromVector(rotMat, event.values.filterIndexed { index, fl -> index < 3 }.toFloatArray())
+        val orientation = FloatArray(3)
+        SensorManager.getOrientation(rotMat, orientation)
+        val degrees = Math.toDegrees(360 - orientation[0].toDouble()).toFloat()
+        compassNeedle.rotation = degrees
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do nothing
     }
 }
