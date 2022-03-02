cb7263dad7fcaf917e08b8075047f22b2146d533
NPSchmidt <93378725+NPSchmidt@users.noreply.github.com>
2022-03-02 19:17:58 +0100

    implemented getting device location

--- app/src/main/java/com/example/group_d/ui/main/ingame/CompassFragment.kt
+++ app/src/main/java/com/example/group_d/ui/main/ingame/CompassFragment.kt
@@ -1,10 +1,13 @@
 package com.example.group_d.ui.main.ingame
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
 import android.os.Bundle
 import android.os.SystemClock
 import android.view.LayoutInflater
@@ -14,12 +17,19 @@ import android.widget.Chronometer
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.group_d.R
 import com.example.group_d.data.model.Game
 import com.example.group_d.databinding.CompassFragmentBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 import java.util.*
 
 class CompassFragment : Fragment(), SensorEventListener {
@@ -40,6 +50,14 @@ class CompassFragment : Fragment(), SensorEventListener {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var lastLocation: Location? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -76,9 +94,34 @@ class CompassFragment : Fragment(), SensorEventListener {
         compassViewModel.loadLocations(String(resources.openRawResource(R.raw.compass_data).readBytes()))
         compassViewModel.runGame.observe(viewLifecycleOwner, this::onGameLoaded)
 
-        compassViewModel.loadRunningGame(args.gameID)
+        requireLocation()
 
         return root
+    }
+
+    private fun requireLocation() {
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            if (it[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                compassViewModel.loadRunningGame(args.gameID)
+            }
+        }
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                compassViewModel.loadRunningGame(args.gameID)
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                )
+            }
+        }
+
     }
 
     fun onGameLoaded(game: Game?) {
@@ -100,10 +143,25 @@ class CompassFragment : Fragment(), SensorEventListener {
             }
         }, 5000)
         waitSymbol.visibility = View.VISIBLE
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        val cancelToken = CancellationTokenSource().token
+        fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, cancelToken)
+            .addOnSuccessListener { location ->
+                lastLocation = location
+            }
     }
 
     private fun waitTimerFinished() {
-        // TODO: Check if the device point in the right direction
+        // TODO: Check if the device points in the right direction
         compassViewModel.nextLocation()
         waitSymbol.visibility = View.INVISIBLE
         waiting = false
