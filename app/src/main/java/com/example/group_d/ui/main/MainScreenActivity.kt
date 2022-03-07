ee329d04a488b7640826125226c073da2a1bac2b
LucasScheerer <lucas@scheerer-nieheim.de>
2022-03-07 13:50:43 +0100

    more cleaning up

--- app/src/main/java/com/example/group_d/ui/main/MainScreenActivity.kt
+++ app/src/main/java/com/example/group_d/ui/main/MainScreenActivity.kt
@@ -97,49 +97,6 @@ class MainScreenActivity : AppCompatActivity() {
         }
     }
 
-    fun sendMessageToTopic(topic: String){
-        /*db.collection(COL_USER).document(otherUID).get().addOnSuccessListener {
-            val token: String = it.data!!["token"] as String
-
-            val message: RemoteMessage = RemoteMessage.Builder(token).addData("type", msgType).build()
-            FirebaseMessaging.getInstance().send(message)
-        }*/
-        val FCM_API = "https://fcm.googleapis.com/fcm/send"
-        val notification = JSONObject()
-        val notifcationBody = JSONObject()
-        try {
-            notifcationBody.put("title", "Enter_title")
-            notifcationBody.put("message", "MSG")   //Enter your notification message
-            notification.put("to", topic)
-            notification.put("data", notifcationBody)
-            Log.e("TAG", "try")
-        } catch (e: JSONException) {
-            Log.e("TAG", "onCreate: " + e.message)
-        }
-        val jsonObjectRequest = object : JsonObjectRequest(FCM_API, notification,
-            Response.Listener<JSONObject> { response ->
-                Log.i("TAG", "onResponse: $response")
-                //msg.setText("")
-            },
-            Response.ErrorListener {
-                //Toast.makeText(this, "Request error", Toast.LENGTH_LONG).show()
-                Log.i("TAG", "onErrorResponse: Didn't work")
-            }) {
-
-            override fun getHeaders(): Map<String, String> {
-                val params = HashMap<String, String>()
-                params["Authorization"] = "key=AAAACHmFr9o:APA91bGypnZZt1R7IHHVja1sOAlualKFFRs4MwrWwdUpmZ4q9yNxVW4ZYDKJ9TiT1MdNwniO6P5At0v7V1yQgU5KkfITMcIGzOCnTnJU7whYoCmCjPCUYZi-pTDSe72RBLXOiHbyjhj9"
-                params["Content-Type"] = "application/json"
-                return params
-            }
-        }
-        requestQueue.add(jsonObjectRequest)
-    }
-
-    val requestQueue: RequestQueue by lazy {
-        Volley.newRequestQueue(this.applicationContext)
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         FirebaseAuth.getInstance().signOut()
