92d43fef7dd969d52633d43612a26e6f609ab8fb
LucasScheerer <lucas@scheerer-nieheim.de>
2022-03-07 14:08:00 +0100

    even more cleaning up

--- app/src/main/java/com/example/group_d/ui/main/MainScreenActivity.kt
+++ app/src/main/java/com/example/group_d/ui/main/MainScreenActivity.kt
@@ -83,20 +83,6 @@ class MainScreenActivity : AppCompatActivity() {
 
     }
 
-    private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            val response = RetrofitInstance.api.postNotification(notification)
-            if (response.isSuccessful){
-                Log.d(TAG, "Response: ${Gson().toJson(response)}")
-            }
-            else {
-                Log.e(TAG, response.errorBody().toString())
-            }
-        } catch (e: Exception){
-            Log.e(TAG, e.toString())
-        }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         FirebaseAuth.getInstance().signOut()
